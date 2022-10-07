27d1bd7d55c37561b5dccdedee702435ebe6c013
cyh0530 <chiayanghuang0@gmail.com>
2022-10-06 21:01:34 -0700

    init repo

---
+++ index.js
@@ -0,0 +1,23 @@
+async function getOffers() {
+    // Find all the "Add to Card" buttons on the page
+    var offerButtons = Array.from(document.querySelectorAll(".offer-cta")).filter(btn => btn.title == "Add to Card");
+    console.log(offerButtons.length)
+    var index;
+    for (index = 0; index < offerButtons.length; ++index) {
+        console.log(`Clicking offer button ${index}`);
+        offerButtons[index].click();
+        // Wait 0.5 seconds to be nice to AMEX servers
+        await new Promise(r => setTimeout(r, 500));
+    }
+}
+const headerDiv = document.querySelector("#offers > div > section.shadow-1.dls-white-bg > span > div")
+
+const allOfferBtn = document.createElement("button")
+allOfferBtn.classList.add("all-offer-btn")
+allOfferBtn.innerText = "Get All Offers"
+allOfferBtn.onclick = getOffers
+if (headerDiv) {
+    headerDiv.appendChild(allOfferBtn)
+} else {
+    console.error("header div not found")
+}
